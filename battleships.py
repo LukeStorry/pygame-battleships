b3b40b61c93318f570f2d2c718581857fd997092
LukeStorry <luke@lukestorry.co.uk>
2018-05-04 02:58:42 +0100

    another update

--- battleships.py
+++ battleships.py
@@ -1,7 +1,7 @@
 
 import sys
 import pygame
-import random.randint, random.choice
+import random
 from enum import Enum
 
 
@@ -39,14 +39,14 @@ class Ship:
 
 
 class Board:
-    def __init__(self, size=10, ship_sizes=[6, 4, 3, 3, 2]):
+    def __init__(self, size, ship_sizes=[6, 4, 3, 3, 2]):
         self.size = size
         self.ship_sizes = ship_sizes
         self.ships_list = []
         self.hits_list = []
         self.misses_list = []
 
-    def is_valid(self, ship: Ship):
+    def is_valid(self, ship):
         for x, y in ship.coordinate_list:
             if x < 0 or y < 0 or x >= self.size or y >= self.size:
                 return False
@@ -55,7 +55,7 @@ class Board:
                 return False
         return True
 
-    def add(self, ship):
+    def add(self, ship: Ship):
         if self.is_valid(ship):
             self.ships_list.append(ship)
             return True
@@ -82,8 +82,8 @@ class Board:
         return True
 
     def shoot(self, x, y):
-        if x is None or y is None:
-            return
+        if x is None or y is None or not self.valid_target(x, y):
+            return False
 
         hit = False
         for ship in self.ships_list:
@@ -96,10 +96,11 @@ class Board:
         else:
             self.misses_list.append((x, y))
 
-    def colour_grid(self, include_ships):
-        grid = [
-            [colours["water"] for _ in range(self.size)]
-            for _ in range(self.size)]
+        return True
+
+    def colour_grid(self, colours, include_ships):
+        grid = [[colours["water"] for _ in range(self.size)]
+                for _ in range(self.size)]
 
         if include_ships:
             for ship in self.ships_list:
@@ -130,7 +131,7 @@ class Board:
 
 
 class PlayerBoard(Board):
-    def __init__(self, display):
+    def __init__(self, board_size, display):
         super().__init__(board_size)
         self.display = display
         self.place_ships()
@@ -141,7 +142,8 @@ class PlayerBoard(Board):
         setup_finished = False
         ship_num = 0
         while not setup_finished:
-            display.show(self, None)
+            self.display.show(None, self)
+
             if ship is None:
                 text = 'Click where you want your {}-long ship to be:'.format(
                     self.ship_sizes[ship_num])
@@ -150,32 +152,31 @@ class PlayerBoard(Board):
                     '{}-long ship:'.format(self.ship_sizes[ship_num + 1])
             else:
                 text = 'Click again to rotate, or here if ready.'
-            display.show_text(text, lower=True)
+            self.display.show_text(text, lower=True)
 
-        click_x, click_y = display.get_input('lower')
-        if click_x is not None and click_y is not None:
-            if ship is not None:  # already a ship in the queue
-                if ship.x == click_x and ship.y == click_y:  # rotate
-                    board.remove(ship)
+            x, y = self.display.get_input()
+            if x is not None and y is not None:
+                if ship is not None:  # already a ship in the queue
+                    if ship.x == x and ship.y == y:  # rotate
+                        self.remove(ship)
+                        ship_direction = ship_direction.next()
+                    else:  # build new
+                        ship_num += 1
+                ship = Ship(x, y, ship_direction, self.ship_sizes[ship_num])
+
+                # Add ship to board, or delete if not valid
+                if not self.add(ship):
                     ship_direction = ship_direction.next()
-                else:  # build new
-                    ship_num += 1
-                    ship = Ship(click_x, click_y, ship_direction,
-                                self.ship_sizes[ship_num])
+                    ship = None
 
-            # Add ship to board, or delete if not valid
-            if board.is_valid(ship):
-                board.add(ship)
-            else:
-                ship_direction = ship_direction.next()
-                ship = None
+            if len(self.ships_list) == len(self.ship_sizes):
+                setup_finished = True
 
-        if len(board.ships_list) == len(self.ship_sizes):
-            setup_finished = True
+            self.display.flip()
 
 
 class AIBoard(Board):
-    def init(self):
+    def init(self, board_size):
         super().__init__(board_size)
         for ship_length in self.ship_sizes:
             shipFound = False
@@ -184,16 +185,11 @@ class AIBoard(Board):
                 y = random.randint(0, board_size - 1)
                 ship_direction = random.choice(list(Direction))
                 ship = Ship(x, y, ship_direction, ship_length)
-
-                if self.is_valid(ship):
-                    self.add(ship)
+                if self.add(ship):
                     shipFound = True
 
 
 class Display:
-    board_size = 10
-    margin = 15
-    cell_size = 40
     colours = {
         "water": (0, 0, 150),
         "ship": (50, 50, 50),
@@ -203,66 +199,71 @@ class Display:
         "text": (255, 255, 255)
     }
 
-    def __init__(self, board_size, cell_size, margin):
-        self.screen = screen
+    def __init__(self, board_size, cell_size=30, margin=15):
         self.board_size = board_size
         self.cell_size = cell_size
         self.margin = margin
 
         pygame.init()
         pygame.font.init()
+        self.font = pygame.font.SysFont("Helvetica", 14)
 
-        screen_width = cell_size * board_size + 2 * margin
-        screen_height = 2 * cell_size * board_size + 3 * margin
+        screen_width = self.cell_size * board_size + 2 * margin
+        screen_height = 2 * self.cell_size * board_size + 3 * margin
         self.screen = pygame.display.set_mode([screen_width, screen_height])
         pygame.display.set_caption("Battleships")
-        return screen
 
     def show(self, upper_board, lower_board, include_top_ships=False):
-        self.screen.fill(colours["background"])
+        if upper_board is not None:
+            upper_colours = upper_board.colour_grid(self.colours, include_top_ships)
+
+        if lower_board is not None:
+            lower_colours = lower_board.colour_grid(self.colours, include_ships=True)
+
+        self.screen.fill(Display.colours["background"])
         for y in range(self.board_size):
             for x in range(self.board_size):
-                grid = upper_board.colour_grid(include_ships=include_top_ships)
-                rectangle = [self.margin + x * self.cell_size,
-                             self.margin + y * self.cell_size,
-                             self.cell_size, self.cell_size]
-                pygame.draw.rect(self.screen, grid[y][x], rectangle)
 
-                grid = lower_board.colour_grid(include_ships=True)
-                offset = self.margin * 2 + self.board_size * cell_size
-                rectangle = [self.margin + x * self.cell_size,
-                             offset + y * self.cell_size,
-                             self.cell_size, self.cell_size]
-                pygame.draw.rect(self.screen, grid[y][x], rectangle)
-        pygame.display.flip()
-        pygame.time.Clock().tick(60)
+                if upper_board is not None:
+                    pygame.draw.rect(self.screen, upper_colours[y][x],
+                                     [self.margin + x * self.cell_size,
+                                     self.margin + y * self.cell_size,
+                                     self.cell_size, self.cell_size])
 
-    def get_input(location):
+                if lower_board is not None:
+                    offset = self.margin * 2 + self.board_size * self.cell_size
+                    pygame.draw.rect(self.screen, lower_colours[y][x],
+                                     [self.margin + x * self.cell_size,
+                                     offset + y * self.cell_size,
+                                     self.cell_size, self.cell_size])
+
+    def get_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                close()
+                self.close()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
-                x = (x - margin) // cell_size
-                if location == 'upper':
-                    y = (y - margin) // cell_size
-                else if location == 'lower':
-                    y = ((y - (board_size * cell_size + 2 * margin)) //
-                         cell_size)
-                else return None, None
-                if x >= 0 and y >= 0 and x < board_size and y < board_size:
+                y = y % (self.board_size * self.cell_size + self.margin)
+                x = (x - self.margin) // self.cell_size
+                y = (y - self.margin) // self.cell_size
+                if x >= 0 and y >= 0 and \
+                        x < self.board_size and y < self.board_size:
                     return x, y
         return None, None
 
-    def show_text(text, upper=False, lower=False):
+    def show_text(self, text, upper=False, lower=False):
         x = self.margin
-        y_up = self.board_size * self.cell_size + self.margin * 2
-        y_lo = x
-        label = self.font.render(text, True, self.colours["text"])
+        y_up = x
+        y_lo = self.board_size * self.cell_size + self.margin
+        label = self.font.render(text, True, Display.colours["text"])
         if upper:
             self.screen.blit(label, (x, y_up))
         if lower:
             self.screen.blit(label, (x, y_lo))
+
+    def flip(self):
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
 
     def close(self):
         pygame.display.quit()
@@ -270,39 +271,32 @@ class Display:
         sys.exit()
 
 
-class Game():
-
-    def __init__(self, display):
-        self.ai_board = AIBoard()
-        self.player_board = PlayerBoard(display)
+class Game:
+    def __init__(self, size, display):
+        self.ai_board = AIBoard(size)
+        self.player_board = PlayerBoard(size, display)
 
     def play(self):
-        while not check_gameover():
-            if player_shoot():
-                ai_shoot()
+        while not self.check_gameover():
+            print("t")
+            if self.player_shoot():
+                self.ai_shoot()
 
             self.display.show(ai_board, player_board)
             self.display.show_text("Click to guess:")
 
-    def ai_shoot():
+    def ai_shoot(self):
         shot = False
         while not shot:
             x = random.randint(0, board_size - 1)
             y = random.randint(0, board_size - 1)
-            if self.player_board.valid_target(x, y):
-                self.player_board.shoot(x, y)
-                shot = True
+            shot = self.player_board.shoot(x, y)
 
-    def player_shoot():
-        x, y = get_input("upper")
-        if x is not None and y is not None:
-            if self.ai_board.valid_target(x, y):
-                self.ai_board.shoot(x, y)
-                return True
-        else:
-            return False
+    def player_shoot(self):
+        x, y = self.get_input()
+        return self.ai_board.shoot(x, y)
 
-    def check_gameover():
+    def check_gameover(self):
         if self.ai_board.gameover():
             print("Congratulations you won")
             return True
@@ -316,8 +310,8 @@ class Game():
 def main():
     replay = True
     while replay:
-        d = Display()
-        g = Game(d)
+        d = Display(10)
+        g = Game(10, d)
         g.play()
         d.close()
 
